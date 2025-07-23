6335e638790f0a015354575a6d689343f90f1df0
Carlos Miguel <c245210@dac.unicamp.br>
2025-07-23 12:47:29 -0300

    Add functionality to calculate and display the new sum after adding a variable

--- TesteGit.py
+++ TesteGit.py
@@ -6,3 +6,8 @@ a = 5
 b = 10
 c = a + b
 print("The sum of", a, "and", b, "is", c)
+print("This script is part of a Git test workflow.")
+# The following line is a placeholder for a commit message
+d = 15
+c = c + d
+print("After adding", d, "the new sum is", c)
