659e4299176bdd1daf81a1cc7a8bb0390c345803
Carlos Miguel <c245210@dac.unicamp.br>
2025-07-23 12:33:33 -0300

    Commit para Soma de a + b

--- TesteGit.py
+++ TesteGit.py
@@ -1,1 +1,8 @@
 print("Hello, World!")
+# This is a simple Python script to print a message
+# It serves as a test for Git functionality
+
+a = 5
+b = 10
+c = a + b
+print("The sum of", a, "and", b, "is", c)
